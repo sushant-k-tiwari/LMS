ec6076644e75be6e4354a7361c119956e884d3b5
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-04-29 22:25:16 +0530

    enrolled course data fetched

--- app/services/index.tsx
+++ app/services/index.tsx
@@ -36,13 +36,20 @@ export const getCourseList = async () => {
 };
 
 export const enrollCourse = async (courseId, userEmail) => {
-  const mutationQuery = gql`
+  const mutationQuery =
+    gql`
     mutation MyMutation {
       createUserEnrolledCourse(
         data: {
-          courseId: "`+courseId+`"
-          userEmail: "`+userEmail+`"
-          course: { connect: { id: "`+courseId+`" } }
+          courseId: "` +
+    courseId +
+    `"
+          userEmail: "` +
+    userEmail +
+    `"
+          course: { connect: { id: "` +
+    courseId +
+    `" } }
         }
       ) {
         id
@@ -57,6 +64,28 @@ export const enrollCourse = async (courseId, userEmail) => {
     }
   `;
   const result = await request(MASTER_URL, mutationQuery);
+
+  return result;
+};
+
+export const getUserEnrolledCourse = async(courseId, userEmail) => {
+  const query = gql`
+    query GetUserEnrolledCourse {
+      userEnrolledCourses(
+        where: {
+          courseId: "`+courseId+`"
+          userEmail: "`+userEmail+`"
+        }
+      ) {
+        id
+        courseId
+        completedChapter {
+          chapterId
+        }
+      }
+    }
+  `;
+  const result = await request(MASTER_URL, query);
 
   return result;
 };
