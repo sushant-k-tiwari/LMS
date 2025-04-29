40a2572da406c88fddb5e0d8bc2216d95f492bd0
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-04-29 22:24:01 +0530

    stylesheet added

--- app/components/CourseDetailSection/ChapterSection.tsx
+++ app/components/CourseDetailSection/ChapterSection.tsx
@@ -1,57 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Colors from "@/app/utils/Colors";
+import { useNavigation } from "@react-navigation/native";
+// import ChapterContentScreen from "@/app/screens/ChapterContentScreen";
 
-const ChapterSection = ({ chapterList }) => {
+const ChapterSection = ({ chapterList, userEnrolledCourse }) => {
+  const navigation = useNavigation();
+  const onChapterPress = (content) => {
+    if (userEnrolledCourse.length == 0) {
+      return;
+    } else {
+      navigation.navigate("chapter-content");
+      content: content;
+    }
+  };
   return (
-    <View
-      style={{
-        padding: 10,
-        marginTop: 20,
-        backgroundColor: "white",
-        borderRadius: 16,
-      }}
-    >
-        <Text style={{fontSize:22, fontFamily:"PopSb"}}>Chapter</Text>
+    <View style={styles.rootContainer}>
+      <Text style={{ fontSize: 22, fontFamily: "PopSb" }}>Chapter</Text>
       {chapterList.map((item, index) => (
-        <View
-        key={item.id || item}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            padding: 15,
-            borderWidth: 1,
-            borderRadius: 16,
-            marginTop: 10,
-            borderColor: "gray",
+        <TouchableOpacity
+          key={item.id || item}
+          style={[styles.chapterWrapper]}
+          onPress={() => {
+            onChapterPress(item.content);
           }}
         >
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-            }}
-          >
-            <Text style={{ fontFamily: "PopMed", fontSize: 27, color: "gray" }}>
-              {index + 1}
-            </Text>
-            <Text
-              style={{
-                fontFamily: "PopReg",
-                fontSize: 20,
-                flexWrap: "wrap",
-                color: "gray",
-              }}
-            >
-              {item.chapter}
-            </Text>
+          <View style={styles.chapterContainer}>
+            <Text style={styles.chapterList}>{index + 1}</Text>
+            <Text style={styles.chapterTitle}>{item.chapter}</Text>
           </View>
-          <Ionicons name="lock-closed" size={25} color="gray" />
-        </View>
+          {userEnrolledCourse == 0 ? (
+            <Ionicons name="lock-closed" size={25} color="gray" />
+          ) : (
+            <Ionicons name="play-circle" size={25} color={Colors.SECONDARY} />
+          )}
+        </TouchableOpacity>
       ))}
     </View>
   );
@@ -59,4 +43,39 @@ const ChapterSection = ({ chapterList }) => {
 
 export default ChapterSection;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    padding: 10,
+    marginTop: 20,
+    backgroundColor: "white",
+    borderRadius: 16,
+  },
+  chapterWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 16,
+    marginTop: 10,
+    borderColor: "gray",
+  },
+  chapterContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  chapterList: {
+    fontFamily: "PopMed",
+    fontSize: 27,
+    color: "gray",
+  },
+  chapterTitle: {
+    fontFamily: "PopReg",
+    fontSize: 20,
+    flexWrap: "wrap",
+    color: "gray",
+  },
+});
